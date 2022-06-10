7bd5027a2515175684df9e0cf330537dde1416d7
duvens larins <duvenslarins@gmail.com>
2022-06-10 10:45:15 -0700

    initial commit

---
+++ profile.js
@@ -0,0 +1,15 @@
+let color = document.getElementById('color')
+let place = document.getElementById('place')
+let ritual = document.getElementById('ritual')
+function favcolor(){
+    alert('black')
+}
+function favplace(){
+    alert('France')
+}
+function favritual(){
+    alert('siiiiiuuuuuuuuu')
+}
+color.addEventListener('click',favcolor)
+place.addEventListener('click',favplace)
+ritual.addEventListener('click',favritual)
